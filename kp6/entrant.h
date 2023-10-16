8f325bc2e77ce8641514ecbee6631600e238f63a
youngrusik <118010656+youngrusik@users.noreply.github.com>
2023-10-16 14:26:01 +0500

    kp6

---
+++ kp6/entrant.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdbool.h>
+
+typedef struct {
+    char surname[30];
+    char initials[3];
+    char gender;
+    int school_num;
+    bool have_medal;
+    int math;
+    int russian;
+    int informatics;
+    bool essay;
+} Entrant;
